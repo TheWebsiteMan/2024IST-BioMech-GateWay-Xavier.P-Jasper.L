581b3ddeee7edbaa677c965e671a2eddd3c41653
TheWebsiteMan <126206539+TheWebsiteMan@users.noreply.github.com>
2023-11-10 10:37:43 +1100

    Update Button.ino

--- XavierP/Button/Button.ino
+++ XavierP/Button/Button.ino
@@ -1,14 +1,28 @@
 //remember to fix joystick with a new var for the x then check that for y then print 
 //only if need neutral debug
 
-static unsigned int myButton = 3;
+static unsigned int myButtonA = 2;
+static unsigned int myButtonB = 4;
+static unsigned int myButtonSt = 7;
+static unsigned int myButtonSe = 8;
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(myButton, INPUT);
+  pinMode(myButtonA, INPUT);
+  pinMode(myButtonB, INPUT);
+  pinMode(myButtonSt, INPUT);
+  pinMode(myButtonSe, INPUT);
 }
 
 void loop() {
-  Serial.print("myButton:");
-  Serial.println(digitalRead(myButton));
+  Serial.print("Buttons: A:");
+  Serial.print(digitalRead(myButtonA));
+  Serial.print(" B:");
+  Serial.print(digitalRead(myButtonB));
+  Serial.print(" St:");
+  Serial.print(digitalRead(myButtonSt));
+  Serial.print(" Se:");
+  Serial.println(digitalRead(myButtonSe));
+  delay(50);
 }
