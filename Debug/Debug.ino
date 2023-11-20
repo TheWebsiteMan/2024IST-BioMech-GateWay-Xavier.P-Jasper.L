f88a04fa6894c4129d67f8391d9968f628f86fd7
TheWebsiteMan <126206539+TheWebsiteMan@users.noreply.github.com>
2023-11-21 10:32:45 +1100

    done testing - major bugs

--- Debug/Debug.ino
+++ Debug/Debug.ino
@@ -1,8 +1,19 @@
+#include "Keyboard.h"
+// use this option for OSX:
+//char ctrlKey = KEY_LEFT_GUI;
+// use this option for Windows and Linux:
+
 void setup() {
+
   Serial.begin(9600);
-  Serial.print("board is working");
+  Serial.print("working");
+  // initialize control over the keyboard:
+  Keyboard.begin();
 }
 
 void loop() {
-  
+  Keyboard.press('n');
+  delay(100);
+  Keyboard.releaseAll();
+  delay(100);
 }
